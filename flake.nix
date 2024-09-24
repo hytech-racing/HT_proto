afcb5ca9cb3baed19ed2e46128abc36b1d5dce4c
Ben Hall <rcmast3r1@gmail.com>
2024-09-24 09:31:13 -0400

    adding versioning

--- flake.nix
+++ flake.nix
@@ -19,6 +19,17 @@
       nanopb_runner = pkgs.writeShellScriptBin "run-nanopb" ''
         #!${pkgs.stdenv.shell}
         export PATH="${pkgs.protobuf}/bin:$PATH"
+        generate_header() {
+          local version_string=$1
+          echo "#ifndef HYTECH_MSGS_VERSION_H" > hytech_msgs_version.h
+          echo "#define HYTECH_MSGS_VERSION_H" >> hytech_msgs_version.h
+          echo "const char *version = \"$version_string\";" >> hytech_msgs_version.h
+          echo "#endif" >> hytech_msgs_version.h
+        }
+
+        if [ "$#" -gt 0 ]; then
+          generate_header "$1"
+        fi
         ${pkgs.nanopb}/bin/nanopb_generator.py -I=./proto hytech_msgs.proto base_msgs.proto
       '';
     in
