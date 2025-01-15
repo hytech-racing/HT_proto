cb58e5c79ced3dffea8d003a0a7b4c80ee84e015
Krish <kittur.krish@gmail.com>
2025-01-14 19:23:49 -0500

    save

--- test.py
+++ test.py
@@ -2,6 +2,15 @@ import argparse
 import json
 
 # Load the json data and print it out
+
+# curl \
+#   -X POST \
+#   -H "Accept: application/vnd.github.v3+json" \
+#   https://api.github.com/repos/octocat/hello-world/dispatches \
+#   -d '{"event_type":"event_type", "client_payload": {"foo": "bar"}}'
+
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('json_data', type=str)
 args = parser.parse_args()
